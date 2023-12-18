e61e83e0e278fa84ac57f8f8893ab214ab83e728
helcv <vladimirhelc@gmail.com>
2023-12-18 20:32:06 +0100

    Adding Router in Angular

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,10 +2,13 @@ import { Component, OnInit, NgModule } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
+
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
@@ -13,7 +16,7 @@ export class NavComponent implements OnInit {
   model: any = {};
   isCollapsed = false;
 
-  constructor (public accountService: AccountService) {
+  constructor (public accountService: AccountService, private router: Router, private toasrt: ToastrService) {
 
   }
 
@@ -24,16 +27,14 @@ export class NavComponent implements OnInit {
 
   login(){
     this.accountService.login(this.model).subscribe({
-      next: response =>{
-        console.log(response);
-      },
-      error: error => console.log(error)
-      
+      next: () => this.router.navigateByUrl('/members'),
+      error: error => this.toasrt.error(error.error)
     })
   }
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
   toggleNavbar(){
