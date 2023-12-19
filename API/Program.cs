bed3629f8b09fa80dd546103d54ab951c6231faf
helcv <vladimirhelc@gmail.com>
2023-12-19 15:37:20 +0100

    Error handling on server side - Exception Middleware

--- API/Program.cs
+++ API/Program.cs
@@ -16,6 +16,8 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseAuthentication();
