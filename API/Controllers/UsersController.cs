d38f042977e3914a530d919c17fa8554b0759c17
helcv <vladimirhelc@gmail.com>
2024-01-09 00:47:18 +0100

    Adding photo option on Api using Cloudinary Api Platform

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -13,11 +13,13 @@ public class UsersController : BaseApiController
 {
     private readonly IUserRepository _userRepository;
     private readonly IMapper _mapper;
+    private readonly IPhotoService _photoService;
 
-    public UsersController(IUserRepository userRepository, IMapper mapper)
+    public UsersController(IUserRepository userRepository, IMapper mapper, IPhotoService photoService)
     {
         _userRepository = userRepository;
         _mapper = mapper;
+        _photoService = photoService;
     }
 
     [AllowAnonymous]
@@ -38,8 +40,7 @@ public class UsersController : BaseApiController
     [HttpPut]
     public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
      {
-        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-        var user = await _userRepository.GetUserByUsernameAsync(username);
+        var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
 
         if (user ==null) return NotFound();
 
@@ -53,8 +54,7 @@ public class UsersController : BaseApiController
     [HttpPost("delete")]
     public async Task<ActionResult> DeleteUser(DeleteDto deleteDto)
      {
-        var username = User.FindFirst(ClaimTypes.NameIdentifier).Value;
-        var user = await _userRepository.GetUserByUsernameAsync(username);
+        var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
 
         if (user == null) return NotFound();
 
@@ -67,10 +67,37 @@ public class UsersController : BaseApiController
             if( user.PasswordHash[i] != computedHash[i])    return Unauthorized("invalid password!");
          }
 
-        var delete = await _userRepository.DeleteUserAsync(username);
+        var delete = await _userRepository.DeleteUserAsync(User.GetUsername());
 
         if (delete) return NoContent();
 
         return BadRequest("Failed to delete user");
+    }
+
+    [HttpPost("add-photo")]
+    public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
+    {
+        var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+
+        if(user == null) return NotFound();
+
+        var result = await _photoService.AddPhotoAsync(file);
+
+        if (result.Error != null) return BadRequest(result.Error.Message);
+
+        var photo = new Photo{
+            Url = result.SecureUrl.AbsoluteUri,
+            PublicId = result.PublicId
+        };
+
+        if (user.Photos.Count == 0) photo.IsMain = true;
+
+        user.Photos.Add(photo);
+
+        if(await _userRepository.SaveAllAsync()) {
+            return CreatedAtAction(nameof(GetUser), new {username = user.UserName}, _mapper.Map<PhotoDto>(photo));
+        }
+
+        return BadRequest("Problem adding photo");
     }
 }
