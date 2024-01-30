b1c8c17cb76b3bd9667b05991be2ca204a2e9146
helcv <vladimirhelc@gmail.com>
2024-01-30 16:01:50 +0100

    Add pagination feature to API

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -24,9 +24,12 @@ public class UsersController : BaseApiController
 
     [AllowAnonymous]
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers(){
+    public async Task<ActionResult<PagedList<MemberDto>>> GetUsers([FromQuery]UserParams userParams){
 
-        var users = await  _userRepository.GetMembersAsync();
+        var users = await  _userRepository.GetMembersAsync(userParams);
+
+        Response.AddPaginationHeader(new PaginationHeader(users.CurrentPage, users.PageSize,
+         users.TotalCount, users.TotalPages));
 
         return Ok(users);
     }
--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -16,11 +16,13 @@ public class UserRepository : IUserRepository
         _mapper = mapper;
     }
 
-    public async Task<IEnumerable<MemberDto>> GetMembersAsync()
+    public async Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams)
     {
-        return await _context.Users
+        var query = _context.Users
         .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-        .ToListAsync();
+        .AsNoTracking();
+
+        return await PagedList<MemberDto>.CreateAsync(query, userParams.PageNumber, userParams.PageSize);
     }
 
     public async Task<MemberDto> GetMemberAsync(string username)
