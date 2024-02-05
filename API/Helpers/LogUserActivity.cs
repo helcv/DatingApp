c73dfc3e433a8b0105efe632bab5ee437f7a8bdc
helcv <vladimirhelc@gmail.com>
2024-02-05 19:49:05 +0100

    Filtering feature fixed, Added simple sorting feature - API and client

---
+++ API/Helpers/LogUserActivity.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace API;
+
+public class LogUserActivity : IAsyncActionFilter
+{
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        var resultContext = await next();
+
+        if (!resultContext.HttpContext.User.Identity.IsAuthenticated) return;
+
+        var userId = resultContext.HttpContext.User.GetUserId();
+
+        var repo = resultContext.HttpContext.RequestServices.GetRequiredService<IUserRepository>();
+        var user = await repo.GetUserByIdAsync(userId);
+        user.LastActive = DateTime.Now;
+        await repo.SaveAllAsync();
+    }
+}
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,12 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
-import { SharedModule } from '../../_modules/shared.module';
-import { Observable, take } from 'rxjs';
 import { Pagination } from 'src/app/_models/pagination';
 import { UserParams } from 'src/app/_models/userParams';
-import { AccountService } from 'src/app/_services/account.service';
-import { User } from 'src/app/_models/user';
+
 
 @Component({
   selector: 'app-member-list',
@@ -18,20 +15,11 @@ export class MemberListComponent implements OnInit{
   members: Member[] = [];
   pagination: Pagination | undefined;
   userParams: UserParams | undefined;
-  user: User | undefined;
   genderList = [{value: 'male', display: 'Males'}, {value: 'female', display: 'Females'}]
 
   
-  constructor(private memberService: MembersService, private accountService: AccountService) {
-    this.accountService.currentUser$.pipe(take(1)).subscribe({
-      next: user => {
-
-        if (user){
-          this.userParams = new UserParams(user);
-          this.user = user;
-        }
-      }
-    })
+  constructor(private memberService: MembersService) {
+    this.userParams = this.memberService.getUserParams();
   }
 
   ngOnInit(): void {
@@ -39,28 +27,28 @@ export class MemberListComponent implements OnInit{
   }
 
   loadMembers() {
-    if (!this.userParams) return;
-
-    this.memberService.getMembers(this.userParams).subscribe({
-      next: response => {
-        if (response.result && response.pagination) {
-          this.members = response.result;
-          this.pagination = response.pagination;
+    if (this.userParams){
+      this.memberService.setUserParams(this.userParams);
+      this.memberService.getMembers(this.userParams).subscribe({
+        next: response => {
+          if (response.result && response.pagination) {
+            this.members = response.result;
+            this.pagination = response.pagination;
+          }
         }
-      }
-    })
+      })
+    }
   }
 
   resetFilters() {
-    if (this.user) {
-      this.userParams = new UserParams(this.user);
-      this.loadMembers();
-    }
+    this.userParams = this.memberService.resetUserParams();
+    this.loadMembers();
   }
 
   pageChanged(event: any) {
     if (this.userParams && this.userParams?.pageNumber !== event.page) {
       this.userParams.pageNumber = event.page;
+      this.memberService.setUserParams(this.userParams);
       this.loadMembers();
     }
   }
